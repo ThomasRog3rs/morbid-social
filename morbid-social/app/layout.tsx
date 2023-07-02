166f68ce240f03173fa0ae7d5ef7ef821a7fe049
Thomas Rogers <42161850+ThomasRog3rs@users.noreply.github.com>
2023-07-02 17:43:24 +0100

    Update profile page created and navbar component
    Created the updated profile page and a navbar component. changed layout file slightly

--- morbid-social/app/layout.tsx
+++ morbid-social/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Navbar from "../components/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Navbar></Navbar>
         <div className="px-20 py-20">{children}</div>
       </body>
     </html>
