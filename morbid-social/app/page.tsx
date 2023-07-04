70998b1aa3c29f08b988a5a66360e1ca98a10fc9
Thomas Rogers <42161850+ThomasRog3rs@users.noreply.github.com>
2023-07-04 23:38:05 +0100

    Added flowbite dependency
    Does not like being used on SSR components, this needs investigationg more.
    Look at:
    https://flowbite.com/docs/getting-started/next-js/
    https://github.com/tulupinc/flowbite-next-starter/

--- morbid-social/app/page.tsx
+++ morbid-social/app/page.tsx
@@ -1,5 +1,12 @@
 import Image from "next/image";
+import { Alert } from "flowbite-react";
+import { Fragment } from "react";
 
 export default function Home() {
-  return <h1>Welcome to the jungle baby, you're gonna die!</h1>;
+  return (
+    <Fragment>
+      <h1>Welcome to the jungle baby, you're gonna die!</h1>
+      <Alert color="info">Alert!</Alert>
+    </Fragment>
+  );
 }
